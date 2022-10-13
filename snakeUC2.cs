dddde604bec99866494d68a0d4523904a9d8722c
AkashNarale <akashnarale43@gmail.com>
2022-10-13 23:42:29 +0530

    merge

---
+++ snakeUC2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class snakeUC2
+    {
+        public static void UC2()
+        {
+                Random random = new Random();
+                int x = random.Next(1, 7);
+                Console.WriteLine("Player1 Roll is :" + x);
+        }
+    }
+}
